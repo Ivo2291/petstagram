6dfb35c1faa9a4bd1ef28c494284ee79632709a6
ivo2291 <ivo2291@gmail.com>
2023-06-06 11:49:46 +0300

    making some changes in accounts app

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
+from petstagram.accounts import views
 
 urlpatterns = (
-
+    path('register/', views.register_account, name='register account'),
+    path('login/', views.login_account, name='login account'),
+    path('profile/<int:pk>/', include([
+        path('', views.details_account, name='details account'),
+        path('edit/', views.edit_account, name='edit account'),
+        path('delete/', views.delete_account, name='delete account'),
+        ]
+    ))
 )
--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 
-def account_register_page(request):
-    pass
+def register_account(request):
+    return render(request, 'accounts/register-page.html')
+
+
+def login_account(request):
+    return render(request, 'accounts/login-page.html')
+
+
+def details_account(request, pk):
+    return render(request, 'accounts/profile-details-page.html')
+
+
+def edit_account(request, pk):
+    return render(request, 'accounts/profile-edit-page.html')
+
+
+def delete_account(request, pk):
+    return render(request, 'accounts/profile-delete-page.html')
