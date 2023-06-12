812be6714df1ecd24db08874583e7ca4e6d21444
ivo2291 <ivo2291@gmail.com>
2023-06-12 11:28:57 +0300

    making changes

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Pet(models.Model):
+    MAX_LENGTH_NAME = 30
+
+    name = models.CharField(
+        max_length=MAX_LENGTH_NAME,
+        null=False,
+        blank=False,
+    )
+
+    personal_pet_photo = models.URLField(
+        null=False,
+        blank=False,
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        blank=True,
+    )
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f"{self.id}-{self.name}")
+
+        return super().save(*args, **kwargs)
--- petstagram/settings.py
+++ petstagram/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram_db',
+        'USER': 'Ivo',
+        'PASSWORD': 'DockerPass6463',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
