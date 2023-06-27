dada860ac876f356c255e1c4e6002e731784c7b7
ivo2291 <ivo2291@gmail.com>
2023-06-27 16:47:32 +0300

    Making some changes

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-&ixc3p1#z59_asveun4ji)r0%naov*@bi@()8ri6u3+=+t*8un'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 INSTALLED_APPS = (
     'django.contrib.admin',
