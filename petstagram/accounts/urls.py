62feab261eae50948485f9c011d365b9fe80fa94
ivo2291 <ivo2291@gmail.com>
2024-01-26 14:53:56 +0200

    Edit some files

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from petstagram.accounts import views as v
 
 urlpatterns = (
-    path('signup/', v.signup_user, name='signup user'),
-    path('signin/', v.signin_user, name='signin user'),
+    path('login/', v.login_user, name='login user'),
+    path('register/', v.register_user, name='register user'),
+    path('logout/', v.logout_user, name='logout user'),
     path('profile/<int:pk>/', include([
         path('', v.details_profile, name='details profile'),
         path('edit/', v.edit_profile, name='edit profile'),
--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
-def signin_user(request):
+def register_user(request):
     context = {}
 
-    return render(request, 'accounts/signin-user.html', context)
+    return render(request, 'accounts/register-user.html', context)
 
 
-def signup_user(request):
+def login_user(request):
     context = {}
 
-    return render(request, 'accounts/signup-user.html', context)
+    return render(request, 'accounts/login-user.html', context)
+
+
+def logout_user(request):
+    return redirect('home page')
 
 
 def details_profile(request, pk):
